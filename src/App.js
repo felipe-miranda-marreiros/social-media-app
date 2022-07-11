e5e608ad648d838e4ec5c43b6de885a9ecedeba9
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-07-10 21:03:35 -0300

    feat: add background blur

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Subscribe Zainkeeps code</div>;
+  return (
+    <div className="App">
+      <div className="blur" style={{ top: '-18%', right: '0' }}></div>
+      <div className="blur" style={{ top: '36%', left: '-8rem' }}></div>
+    </div>
+  );
 }
 
 export default App;

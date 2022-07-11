a687276f57d271ee17a5db3fede0fbcdd6ce23e3
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-07-10 21:34:21 -0300

    feat: LogoSearch component

---
+++ src/components/logoSearch/LogoSearch.jsx
@@ -0,0 +1,19 @@
+import './LogoSearch.css';
+import Logo from '../../img/logo.png';
+import { UilSearch } from '@iconscout/react-unicons';
+
+const LogoSearch = () => {
+  return (
+    <div className="LogoSearch">
+      <img src={Logo} alt="Logo" />
+      <div className="Search">
+        <input type="text" placeholder="#Explorer" />
+        <div className="s-icon">
+          <UilSearch />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LogoSearch;

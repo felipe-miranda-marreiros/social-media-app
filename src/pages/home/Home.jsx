e6b1fcf77c393579c6a05154240057b31e63b804
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-07-11 22:54:51 -0300

    chore: add ProfileSide and RightSide to Home container

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
+import PostSide from '../../components/postSide/PostSide';
 import ProfileSide from '../../components/profileSide/ProfileSide';
+import RightSide from '../../components/rightSide/RightSide';
 import './Home.css';
 
 const Home = () => {
   return (
     <div className="Home">
       <ProfileSide />
-      <div className="postSide">Posts</div>
-      <div className="rightSide">Rightside</div>
+      <PostSide />
+      <RightSide />
     </div>
   );
 };

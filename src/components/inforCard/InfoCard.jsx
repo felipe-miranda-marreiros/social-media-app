064dec0e04d0344f6f29197339f3d1d4f25e6da4
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-07-13 15:39:53 -0300

    feat: InfoCard component

---
+++ src/components/inforCard/InfoCard.jsx
@@ -0,0 +1,42 @@
+import './InfoCard.css';
+import { UilPen } from '@iconscout/react-unicons';
+
+const InfoCard = () => {
+  return (
+    <div className="InfoCard">
+      <div className="infoHead">
+        <h4>Your Info</h4>
+        <div>
+          <UilPen width="2rem" height="1.2rem" />
+        </div>
+      </div>
+
+      <div className="info">
+        <span>
+          <b>Status </b>
+        </span>
+        <span>in Relationship</span>
+      </div>
+
+      <div className="info">
+        <span>
+          <b>Lives in </b>
+        </span>
+        <span>Multan</span>
+      </div>
+
+      <div className="info">
+        <span>
+          <b>Works at </b>
+        </span>
+        <span>Facebook</span>
+      </div>
+
+      <button type="button" className="button logout-button">
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default InfoCard;

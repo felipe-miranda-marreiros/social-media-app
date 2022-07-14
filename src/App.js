6f4149791a87de2fe608ca78255dc98ea9ff9666
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-07-14 00:29:37 -0300

    chore: show Profile page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import Home from './pages/home/Home';
+import Auth from './pages/Auth/Auth';
+import Home from './pages/Home/Home';
 import Profile from './pages/Profile/Profile';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div className="blur" style={{ top: '36%', left: '-8rem' }}></div>
       {/* <Home /> */}
       <Profile />
+      {/* <Auth /> */}
     </div>
   );
 }
